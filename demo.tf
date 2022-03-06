1debc271b08acb552f8b110a6969b1119f700a68
RUCHI JAIN <76520251+Ruchi-119@users.noreply.github.com>
2022-03-07 00:11:43 +0530

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -12,11 +12,4 @@ resource "azurerm_resource_group" "rg" {
     location = "westus"
 }
 
-resource "azurerm_managed_disk" "catdisk" {
-  name                 = "DevOpsLab_MD"
-  location             = "westus"
-  resource_group_name  = "DevOpsLab_Ruddy"
-  storage_account_type = "Premium_LRS"
-  create_option        = "Empty"
-  disk_size_gb         = "512"
-}
+
